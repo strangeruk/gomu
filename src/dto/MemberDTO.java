8322e5012fde4c10a037779526c50a8cac736b53
JinseobKim <hsjinseob@naver.com>
2015-07-01 10:52:54 +0900

    setting complete
    dto 패키지에는 dto만
    sql 패키지에는 sql xml파일만 넣도록

---
+++ src/dto/MemberDTO.java
@@ -0,0 +1,70 @@
+package dto;
+
+import java.sql.Timestamp;
+
+public class MemberDTO {
+	private String id;
+	private String pw;
+	private String name;
+	private String addr;
+	private int phone;
+	private String email;
+	private Timestamp birth;
+	private String sex;
+	private Timestamp join_date;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Timestamp getBirth() {
+		return birth;
+	}
+	public void setBirth(Timestamp birth) {
+		this.birth = birth;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Timestamp getJoin_date() {
+		return join_date;
+	}
+	public void setJoin_date(Timestamp join_date) {
+		this.join_date = join_date;
+	}
+
+}
